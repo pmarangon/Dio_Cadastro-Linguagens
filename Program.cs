1b977d4b7c6af1fe4937e5b46cf87e33855fdcbc
Paula Masson Marangon <56054345+pmarangon@users.noreply.github.com>
2021-09-19 22:44:29 -0300

    Add files via upload
    Código escrito em
    Criando um APP simples de cadastro de séries em .NET
    Conteúdo incrível, aprendi muito de orientação a objeto na prática, preciso rever as aulas e o código pra refinar, essa versão é só um teste não considero finalizado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Dio.Languages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Language meuObjeto = new Language();
+            meuObjeto.i
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
